95ff066ed247ad840544cfb76ebb4f7a4489d979
DetGrey <anenovlar@gmail.com>
2023-12-04 16:33:54 +0100

    Database connected

---
+++ src/firestore.js
@@ -0,0 +1,58 @@
+import {initializeApp} from "firebase/app";
+import {collection, setDoc, doc, getFirestore, query, where, getDocs } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDQhW9ZGoQNsEOYsXK6MolkRgzupxOgL8E",
+    authDomain: "world-of-pictures.firebaseapp.com",
+    projectId: "world-of-pictures",
+    storageBucket: "world-of-pictures.appspot.com",
+    messagingSenderId: "497847256484",
+    appId: "1:497847256484:web:d4e29809a90966303ead31"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+// -------------------------------------------------- CODE BEGINNING
+const picturesRef = collection(db, "pictures");
+
+const albumsRef = collection(db, "albums");
+
+const recentPictures = document.querySelector('#recent-pictures');
+
+async function retrievePictures (collectionRef) {
+    try
+    {
+        let q = query(collectionRef);
+        let querySnapshot = await getDocs(q);
+        querySnapshot.forEach(doc => {
+            console.log(doc.data);
+            const img = document.createElement('img');
+            img.src = doc.data().img_path;
+            recentPictures.appendChild(img);
+        });
+    }
+    catch(err)
+    {console.log(err)}
+}
+retrievePictures(picturesRef);
+// const firebase_delete_data = async (category, response, product_name) => {
+//     try
+//     {
+//         await db.collection(category).doc(product_name).delete()
+//     }
+//     catch(err)
+//     {console.log(err)}
+// }
+// const firebase_update_data = async (category, response, reqbody) => {
+//     try
+//     {
+//         await db.collection(category).doc(reqbody["productName"]).update({"Price": parseFloat(reqbody["price"]), "Quantity": parseFloat(reqbody["quantity"]), "ExpiryDate": reqbody["expiryDate"]})
+//     }
+//     catch(err)
+//     {
+//         console.log(err)
+//     }
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['./src/firestore.js', './src/index.js', './src/leaflet.js'],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-    },
+    }
 };
